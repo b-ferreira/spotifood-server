9fc0d699970b990ed2415d255eb8a0603a87cda3
Bruno Ferreira <bferreira.assis@gmail.com>
2018-03-12 05:57:41 -0300

    Finishing server v1

--- src/http/modules/spotify.js
+++ src/http/modules/spotify.js
@@ -19,15 +19,43 @@ module.exports = function authenticate (server) {
     next()
   })
 
-  server.get(`${URL_PREFIX}/featured-playlists`, async (req, res, next) => {
-    try {
-      const { body } = await spotifyApi.getFeaturedPlaylists({
-        ...req.query
-      })
-      res.send(200, body)
-    } catch (err) {
-      res.send(500, err)
-    }
-    next()
-  })
+  server.get(`${URL_PREFIX}/featured-playlists`,
+    // Attempts to login on Spotify before requesting the featured playlists.
+    async (req, res, next) => {
+      if (!spotifyApi.getAccessToken()) {
+        try {
+          const { body } = await spotifyApi.clientCredentialsGrant()
+          spotifyApi.setAccessToken(body.access_token)
+        } catch (err) {
+          res.send(500, err)
+          next(false)
+        }
+      }
+      next()
+    },
+    // Requests the featured playlists
+    async (req, res, next) => {
+      try {
+        const { body } = await spotifyApi.getFeaturedPlaylists({
+          ...req.query
+        })
+        res.send(200, {
+          message: body.message,
+          playlists: body.playlists.items
+        })
+      } catch (err) {
+        // In case the token has been expired, we clear our current token.
+        // It'll be loaded again in the next time we consume the API.
+        if (err.statusCode === 401) {
+          spotifyApi.setAccessToken(null)
+          res.send(401, {
+            error: true,
+            message: 'Expired token.'
+          })
+        } else {
+          res.send(500, err)
+        }
+      }
+      next()
+    })
 }
