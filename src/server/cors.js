505979e6b915a7d4add068ea87426382daeae016
Bruno Ferreira <bferreira.assis@gmail.com>
2018-03-13 07:08:45 -0300

    CORS updated

--- src/server/cors.js
+++ src/server/cors.js
@@ -3,7 +3,7 @@ const corsMiddleware = require('restify-cors-middleware')
 
 const cors = corsMiddleware({
   preflightMaxAge: 5, // Optional
-  origins: ['*'],
+  origins: ['http://ifood-frontend-test.herokuapp.com', 'https://ifood-frontend-test.herokuapp.com'],
   allowHeaders: ['GET'],
   exposeHeaders: ['GET']
 })
